4aabc23454927799c0423c61ebcf987e243a2d4c
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-09-21 16:54:05 +0600

    sql query updated

--- DATABASEBACKUP/SQLQuery1.sql
+++ DATABASEBACKUP/SQLQuery1.sql
@@ -1,1 +1,5 @@
 select * from CUSTOMERINFO
+
+insert into CUSTOMERINFO(CustomerName,CustomerAddress,
+CustomerEmail,CustomerPassword,CustomerPhone,CustomerArea,CustomerCity)
+values('Fahim','Dhaka','f1@gmail.com','123456','01873974','ABc','Dhaka')
